9fbf9571e86437025c876812254a307fb875920f
genturarif <gentur1608@gmail.com>
2021-07-04 14:21:20 +0700

    menambah jarak page y offset

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let nav = document.querySelector("nav");
 window.addEventListener("scroll", function () {
-  if (window.pageYOffset > 100) {
+  if (window.pageYOffset > 350) {
     nav.classList.add("bg-navbar", "shadow");
   } else {
     nav.classList.remove("bg-navbar", "shadow");
